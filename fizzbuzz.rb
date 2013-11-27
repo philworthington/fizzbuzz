28a228a6bc0bbb0e25bcbe7901fe3d213f53bbb1
Phil Worthington <pworthington27@gmail.com>
2013-11-26 19:29:16 -0800

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,12 @@
-(1...100).each do |num|
-  mult_of_three = (num % 3) == 0
-  mult_of_five = (num % 5) == 0
-  mult_of_both = mult_of_three && mult_of_five
-  if mult_of_both
-    puts "FizzBuzz"
-  elsif mult_of_three
-    puts "Fizz"
-  elsif mult_of_five
-    puts "Buzz"
+def fizzbuzz(number)
+  if number % 5 == 0 && number % 3 == 0
+    puts "fizzbuzz"
+  elsif number % 3 == 0
+    puts "fizz"
+  elsif number % 5 == 0
+    puts "buzz"
   else
-    puts num
+    puts number
   end
 end
 
